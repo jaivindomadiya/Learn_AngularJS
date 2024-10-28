2f8d0b3b7aa28df93b0e2bf900327fde7b605bd6
Jaivin Domadiya <Jaivindomadiya@gmail.com>
2024-10-28 23:16:39 +0530

    Day-1 Work Done (Know about Basic overview)

--- Basic_overview_on_Youtube/my-first-project/src/app/app.component.ts
+++ Basic_overview_on_Youtube/my-first-project/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+import {Todo} from "./todo"
+import { ValueChangeEvent } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +13,30 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'My First Project In Angular';
+
+  todovalue:string = "";
+  list :Todo[] = [];
+
+  ngOnInit(){
+    this.list = [];
+    this.todovalue = "";
+  }
+
+  addItem(){
+     if (this.todovalue !== ""){
+      const newItem : Todo = {
+        id :Date.now(),
+        value:this.todovalue,
+        isDone: false
+      };
+      this.list.push(newItem);
+
+     }
+     this.todovalue = "";
+  }
+  deleteItem(id:number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
+
 }
